a6a3f92360b321d4df158299ce3c45af76b63004
Jaleron <jaleronmail@gmail.com>
2022-05-29 19:27:49 -0400

    Added flag and fieldSVG declarations, flag width and height declarations, and assigned every width and height to the flag div's width and height

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const flag = document.getElementById('flag');
 const field = document.getElementById('field');
+const fieldSVG = document.getElementById('fieldSVG');
+
+let flagWidth = 360;
+let flagHeight = 240;
+
+flag.style.width = `${flagWidth}px`;
+flag.style.height = `${flagHeight}px`;
+
+field.style.width = `${flagWidth}px`;
+field.style.height = `${flagHeight}px`;
+
+fieldSVG.style.width = `${flagWidth}px`;
+fieldSVG.style.height = `${flagHeight}px`;
